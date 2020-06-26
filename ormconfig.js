ec3cd7932d036dd15993eeba5e6a2421e350fa97
Takuto Mitsuhashi <picos.tackt@gmail.com>
2020-06-26 13:27:40 +0900

    Create first migrations

---
+++ ormconfig.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = {
+  type: 'mysql',
+  host: 'db',
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  entities: ['src/model/**/*.ts'],
+  migrations: ['src/storage/migration/**/*.ts'],
+  subscribers: ['src/storage/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/model',
+    migrationsDir: 'src/storage/migration',
+    subscribersDir: 'src/storage/subscriber',
+  },
+}
