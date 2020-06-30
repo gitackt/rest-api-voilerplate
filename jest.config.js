65726e6d07b85225a34743791391bcca60c40a33
Takuto Mitsuhashi <picos.tackt@gmail.com>
2020-06-30 18:21:46 +0900

    Fix jest path settings

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': ['<rootDir>/__test__/$1'],
+    '@api/(.*)': ['<rootDir>/src/app/api/$1'],
+    '@model/(.*)': ['<rootDir>/src/app/model/$1'],
+    '@storage/(.*)': ['<rootDir>/src/app/storage/$1'],
+    '@logger/(.*)': ['<rootDir>/src/logger/$1'],
+  },
 }
